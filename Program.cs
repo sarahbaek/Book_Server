58d557d94185d5c8524771371b06b0c8e9ca8fc7
sarahBæk2021 <sbaek18@gmail.com>
2021-10-06 20:12:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.ConstrainedExecution;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Book_Library;
+
+namespace Book_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Book Server");
+
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 4646);
+
+            listener.Start();
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("Incoming client");
+                Task.Run((() => { HandleClient(socket); }));
+
+            }
+
+        }
+
+        private static void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            string message = reader.ReadLine();
+
+            Book FromJsonBook = JsonSerializer.Deserialize<Book>(message);
+
+            Console.WriteLine("Client sent:" + FromJsonBook.Title + FromJsonBook.Author + FromJsonBook.PageNumber + FromJsonBook.ISBN13);
+
+            writer.WriteLine("Book received");
+            writer.Flush();
+            socket.Close();
+
+        }
+    }
+}
